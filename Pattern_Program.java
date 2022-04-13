ce449b29fc188ff7d9e0dde6243a327dc2385a1a
Karthiga Jayaperumal <99970828+KarthigaJayaperumal@users.noreply.github.com>
2022-04-13 11:53:10 +0530

    Pattern_Programs

--- Pattern_Program.java
+++ Pattern_Program.java
@@ -3,18 +3,45 @@ package april_program;
 public class Pattern_Program {
 	
 	public static void main(String[] args) {
-		int no=1;
+		//Pattern 1
 		for(int row=1; row<=5;row++) {
-			 
 			for(int col=5;col>=row;col--) {
-			System.out.print(no+"   ");
-			no++;
-			}
-			if(no>15)
-			{
-				break;
+			System.out.print(col+"   ");
 			}
 			System.out.println();
-			}}}
-
+			}
+		//Pattern 2
+for(int row=1; row<=5;row++) {
+	for(int col=5;col>=row;col--) {
+	System.out.print(row+"   ");
+	}
+	System.out.println();
+	}
+//Pattern 3
+for(int row=5; row>=1;row--) {
+	for(int col=1;col<=row;col++) {
+	System.out.print(row+"   ");
+	}
+	System.out.println();
+	}
+//Pattern 4
+for(int row=5; row>=1;row--) {
+	for(int col=1;col<=row;col++) {
+	System.out.print(row+"   ");
+	}
+	System.out.println();
+	}
+int no=1;
+for(int row=1; row<=5;row++) {
+	 
+	for(int col=5;col>=row;col--) {
+	System.out.print(no+"   ");
+	no++;
+	}
+	if(no>15)
+	{
+		break;
+	}
+	System.out.println();
+	}}}
 
